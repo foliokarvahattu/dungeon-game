519351a53f540e02d2fc51c5a098037f39bbdff8
foliokarvahattu <eliashinkkanen@gmail.com>
2018-10-08 13:46:23 +0300

    Added destructor

--- enemy.hpp
+++ enemy.hpp
@@ -1,17 +1,18 @@
 #ifndef ENEMY_HH
 #define ENEMY_HH
 #include <iostream>
-#include <vector>
 
 class Enemy {
 public:
-    Enemy(std::string const &name, std::string const &type);
-	const std::string& getName() const;
+    Enemy(std::string const &name, std::string const &type, int hpoints, int mpoints): name_(name),type_(type), hpoints_(hpoints), mpoints_(mpoints) { }
+	virtual ~Enemy();
+    const std::string& getName() const;
 	const std::string& getType() const;
 	int getHP() const;
 private:
-    std::string name;
-    int hpoints;
-    int mpoints;
+    std::string const name_;
+    std::string const type_;
+    int hpoints_;
+    int mpoints_;
 };
 #endif
